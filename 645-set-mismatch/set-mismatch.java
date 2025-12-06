4eeae14cbcdb6b4384e6ee209e8d5178e433127c
Harsh Chauhan <harshchauhan77423@gmail.com>
2025-12-06 23:49:44 +0530

    Time: 2 ms (93.46%) | Memory: 47.6 MB (49.88%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,35 +1,52 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int n = nums.length;
-        int dupe = -1, miss = -1;
+//         int n = nums.length;
+//         int dupe = -1, miss = -1;
 
-for(int i=0 ; i < n ; i++){
-    int count = 0;
-    for(int j=0 ; j < n ; j++){
-        if(nums[j] == nums[i])count++;
+// for(int i=0 ; i < n ; i++){
+//     int count = 0;
+//     for(int j=0 ; j < n ; j++){
+//         if(nums[j] == nums[i])count++;
 
-    }
-    if(count == 2){
-        dupe = nums[i];
-        break;
-    }
+//     }
+//     if(count == 2){
+//         dupe = nums[i];
+//         break;
+//     }
 
+// }
+
+// for(int k=1; k <= n ; k++){
+//     boolean present = false;
+//     for(int j=0 ; j < n ; j++){
+//         if(nums[j] == k){
+//             present = true;
+//             break;
+//         }
+//     }
+//     if(!present){
+//         miss = k;
+//         break;
+//     }
+// }
+// return new int[]{dupe , miss};
+
+int[] count = new int[nums.length + 1];
+int[] ans = new int[2];
+
+for(int num : nums){
+    count[num]++;
 }
 
-for(int k=1; k <= n ; k++){
-    boolean present = false;
-    for(int j=0 ; j < n ; j++){
-        if(nums[j] == k){
-            present = true;
-            break;
-        }
+for(int i=1; i <= nums.length ; i++){
+    if(count[i] == 2){
+        ans[0] = i;
     }
-    if(!present){
-        miss = k;
-        break;
+    if(count[i] == 0){
+        ans[1] = i;
+
     }
 }
-return new int[]{dupe , miss};
-
+return ans;
     }
 }
