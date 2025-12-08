3f175c71c52bbd98fbf37dcc44b5f487c9e0a384
Harsh Chauhan <harshchauhan77423@gmail.com>
2025-12-08 16:56:11 +0530

    Time: 1 ms (22.82%) | Memory: 42.9 MB (28.55%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -2,43 +2,43 @@ class Solution {
     public int strStr(String haystack, String needle) {
         // return haystack.indexOf(needle);
 
-// int k = needle.length();
+int k = needle.length();
 
-// int j= 0;
-// int i =0 ;
-// while(j < haystack.length()){
-//     if(j - i+1 == k){
-//         String s1 = haystack.substring(i , j+1);
-//         if(needle.equals(s1)){
-//             return i;
+int j= 0;
+int i =0 ;
+while(j < haystack.length()){
+    if(j - i+1 == k){
+        String s1 = haystack.substring(i , j+1);
+        if(needle.equals(s1)){
+            return i;
 
-//         }
-//         i++;
+        }
+        i++;
 
-//     }
-//     j++;
+    }
+    j++;
 
-// }
-// return -1;
-
-int n = haystack.length();
-int m = needle.length();
-
-for(int i=0; i <= n-m ; i++){
-    int k=0;
-
-for( k=0; k < m ; k++){
-
-if(haystack.charAt(i+k) != needle.charAt(k)){
-    break;
 }
-}
-
-// if(s1[i++] != s2[j]) break;}
-if(k == m) {
-return i;}
-}
-
 return -1;
+
+// int n = haystack.length();
+// int m = needle.length();
+
+// for(int i=0; i <= n-m ; i++){
+//     int k=0;
+
+// for( k=0; k < m ; k++){
+
+// if(haystack.charAt(i+k) != needle.charAt(k)){
+//     break;
+// }
+// }
+
+// // if(s1[i++] != s2[j]) break;}
+// if(k == m) {
+// return i;}
+// }
+
+// return -1;
     }
 }
