c9176094fb7da4d5cc2f335e9f4854f497fdd46f
Harsh Chauhan <harshchauhan77423@gmail.com>
2026-04-09 00:33:50 +0530

    Time: 7 ms (36.13%) | Memory: 45 MB (55.98%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,20 +1,30 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        for(int num : nums){
-            if(!pq.contains(num)){
-                if(pq.size() < 3){
-                    pq.add(num);
-                }else if(num > pq.peek()){
-                    pq.poll();
-                    pq.add(num);
-                }
-            }
-        }
-        if(pq.size() < 3){
-            while(pq.size() > 1)pq.poll();
-        }
-        return pq.peek();
+        // for(int num : nums){
+        //     if(!pq.contains(num)){
+        //         if(pq.size() < 3){
+        //             pq.add(num);
+        //         }else if(num > pq.peek()){
+        //             pq.poll();
+        //             pq.add(num);
+        //         }
+        //     }
+        // }
+        // if(pq.size() < 3){
+        //     while(pq.size() > 1)pq.poll();
+        // }
+        // return pq.peek();
+
+int count = 0;
+Arrays.sort(nums);
+for(int i=nums.length -1 ; i> 0 ;i--){
+    if(nums[i] != nums[i-1])count++;
+if(count == 2)return nums[i-1];
+
+}
+return nums[nums.length - 1];
+
     }
 }
